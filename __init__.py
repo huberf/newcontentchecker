cbb2a4b34011961d4aa8a6429fb444801b2424d2
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-08-06 10:41:00 -0400

    Init commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Updates:
+  def checkData(self, url, data, place, itemOccurence, debug=False):
+    contents = requests.get(url)
+    contents = contents.text
+    soup = BeautifulSoup(contents, "html.parser")
+    if debug:
+      print soup(place)[itemOccurence].string
+    if soup(place)[itemOccurence].string == data:
+      return False
+    else:
+      return True
